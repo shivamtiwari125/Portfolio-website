a422aea8c82b10ac33a98381623588ba890171c0
Shivam Tiwari <152903019+shivamtiwari125@users.noreply.github.com>
2024-09-06 22:59:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
 let count = 0;
 let button = document.querySelector(".dark-mode");
 let body = document.querySelector("body");
-let rightBox = document.querySelector(".rightbox");
+let rightBox = document.querySelectorAll(".rightbox");
 let about = document.querySelector("#about-head");
 let project = document.querySelector("#pro-head");
 let darkLight = document.querySelector(".dark-light");
+let des = document.querySelector(".proj-descr1");
 
 button.addEventListener("click", () =>{
     count++;
     console.log("clicked");
 
     if(count % 2 == 0){
-        body.style.backgroundColor = "white";
-        rightBox.style.color = "black";
-        about.style.color = "black";
-        project.style.color = "black";
-        button.style.backgroundColor = "black";
-        darkLight.src = "dark mode.png";
-        
-    }
-    else{
-        body.style.backgroundColor = "black";
-        rightBox.style.color = "white";
+        body.style.backgroundColor = " #8582827a";
+         for (const right of rightBox) {
+            right.style.color = "black";
+         }
+         
+         about.style.color = "black";
+         project.style.color = "black";
+         button.style.backgroundColor = "black";
+         darkLight.src = "dark mode.png";
+         
+         
+        }
+        else{
+            body.style.backgroundColor = "rgb(0, 0, 0, 0.99)";
+            for (const right of rightBox) {
+               
+               right.style.color = "white";
+            }
         about.style.color = "white";
         project.style.color = "white";
         button.style.backgroundColor = "white";
         darkLight.src = "light mode.png";
+        // des.style.color = "white";
         
 
     }
 });
 
 
+// const animatedDiv = document.querySelector('');
+// animatedDiv.addEventListener("mouseenter", () =>{
+   
+//     animatedDiv.classList.add("visible");
+    
+// });
+// document.addEventListener("mouseleave", () => {
+    
+//     animatedDiv.classList.remove("visible");
+    
+// });
